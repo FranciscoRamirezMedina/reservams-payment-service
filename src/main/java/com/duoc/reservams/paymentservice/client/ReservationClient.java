6b8d914deaccd67a1ffedaa89a0f53d6f2bbbe94
Francisco Ramírez <TUEMAIL@gmail.com>
2026-07-08 23:38:00 -0400

    fix: configure reservation client url for docker

--- src/main/java/com/duoc/reservams/paymentservice/client/ReservationClient.java
+++ src/main/java/com/duoc/reservams/paymentservice/client/ReservationClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 
 // cliente Feign para comunicarse con reservation-service
-@FeignClient(name = "reservams-reservation-service")
+@FeignClient(name = "reservams-reservation-service", url = "${reservation.service.url:http://localhost:8086}")
 public interface ReservationClient {
 
     // Llama al endpoint que confirma una reserva
